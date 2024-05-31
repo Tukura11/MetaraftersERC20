237f4a0e248f23483a0609d9f852d26cee92008f
Tukura11 <fortuneappen@gmail.com>
2024-05-31 12:23:32 +0100

    Update Assesment.sol

--- contracts/Assesment.sol
+++ contracts/Assesment.sol
@@ -1,18 +1,27 @@
 // SPDX-License-Identifier: MIT
-// Compatible with OpenZeppelin Contracts ^5.0.0
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract FortuneToken is ERC20, ERC20Burnable, Ownable {
-    constructor(address initialOwner)
-        ERC20("FortuneToken", "FTK")
-        Ownable(initialOwner)
-    {}
+contract MyToken is ERC20 {
+    address public owner;
 
-    function mint(address to, uint256 amount) public onlyOwner {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        owner = msg.sender;
+        _mint(msg.sender, 1000000 * 10 ** uint(decimals()));
+    }
+
+    function transfer(address to, uint256 amount) public override returns (bool) {
+        _transfer(msg.sender, to, amount);
+        return true;
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+
+    function mint(address to, uint256 amount) public {
+        require(msg.sender == owner, "Only owner can mint");
         _mint(to, amount);
     }
 }
